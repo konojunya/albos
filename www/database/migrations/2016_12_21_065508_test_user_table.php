cc51beaa4cb89e29f3dec05021517434eef4c070
kinokoruumu <kinokoruumu416@yahoo.co.jp>
2016-12-21 16:23:15 +0900

    migration

--- www/database/migrations/2016_12_21_065508_test_user_table.php
+++ www/database/migrations/2016_12_21_065508_test_user_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class TestUserTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,13 +12,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
+        Schema::create('test_user', function (Blueprint $table) {
+            $table->string('user_id')->unique();
+            $table->string('user_name');
             $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->string('credit_card_number');
+            $table->string('mail_address');
         });
     }
 
@@ -29,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('users');
+        Schema::drop('test_user');
     }
 }
