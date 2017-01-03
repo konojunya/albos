e6486480bd30fdb8a40bb720a6f0cb35d501c1ba
kinokoruumu <kinokoruumu416@yahoo.co.jp>
2017-01-04 01:00:07 +0900

    sqlファイル編集

--- Sqlfiles/music.sql
+++ Sqlfiles/music.sql
@@ -1,18 +1,18 @@
-insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000001', '0000000001', 'いえない', '歌詞', '250', 'data_path', '0:6:32');
-insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000001', '0000000002', '実況中継', '歌詞', '250', 'data_path', '0:5:05');
-insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000001', '0000000003', 'アイアンバイブル', '歌詞', '250', 'data_path', '0:5:35');
-insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000001', '0000000004', 'リユニオン', '歌詞', '250', 'data_path', '0:5:19');
-insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000001', '0000000005', 'DARMA GRAND PRIX', '歌詞', '250', 'data_path', '0:4:09');
-insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000001', '0000000006', '五月の蝿', '歌詞', '250', 'data_path', '0:5:02');
-insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000001', '0000000007', '最後の晩餐', '歌詞', '250', 'data_path', '0:5:10');
-insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000001', '0000000008', '夕霧', '歌詞', '250', 'data_path', '0:1:40');
-insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000001', '0000000009', 'ブレス', '歌詞', '250', 'data_path', '0:6:22');
-insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000001', '0000000010', 'パーフェクトベイビー', '歌詞', '250', 'data_path', '0:3:56');
-insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000001', '0000000011', 'ドリーマーズ・ハイ', '歌詞', '250', 'data_path', '0:5:57');
-insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000001', '0000000012', '会心の一撃', '歌詞', '250', 'data_path', '0:4:36');
-insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000001', '0000000013', 'Tummy', '歌詞', '250', 'data_path', '0:4:47');
-insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000001', '0000000014', 'ラストバージン', '歌詞', '250', 'data_path', '0:5:13');
-insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000001', '0000000015', '針と棘', '歌詞', '250', 'data_path', '0:7:20');
+insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000001', '0000000001', 'いえない', '歌詞', '250', 'music_data/0000000001.mp3', '0:6:32');
+insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000001', '0000000002', '実況中継', '歌詞', '250', 'music_data/0000000001.mp3', '0:5:05');
+insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000001', '0000000003', 'アイアンバイブル', '歌詞', '250', 'music_data/0000000001.mp3', '0:5:35');
+insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000001', '0000000004', 'リユニオン', '歌詞', '250', 'music_data/0000000001.mp3', '0:5:19');
+insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000001', '0000000005', 'DARMA GRAND PRIX', '歌詞', '250', 'music_data/0000000001.mp3', '0:4:09');
+insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000001', '0000000006', '五月の蝿', '歌詞', '250', 'music_data/0000000001.mp3', '0:5:02');
+insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000001', '0000000007', '最後の晩餐', '歌詞', '250', 'music_data/0000000001.mp3', '0:5:10');
+insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000001', '0000000008', '夕霧', '歌詞', '250', 'music_data/0000000001.mp3', '0:1:40');
+insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000001', '0000000009', 'ブレス', '歌詞', '250', 'music_data/0000000001.mp3', '0:6:22');
+insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000001', '0000000010', 'パーフェクトベイビー', '歌詞', '250', 'music_data/0000000001.mp3', '0:3:56');
+insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000001', '0000000011', 'ドリーマーズ・ハイ', '歌詞', '250', 'music_data/0000000001.mp3', '0:5:57');
+insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000001', '0000000012', '会心の一撃', '歌詞', '250', 'music_data/0000000001.mp3', '0:4:36');
+insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000001', '0000000013', 'Tummy', '歌詞', '250', 'music_data/0000000001.mp3', '0:4:47');
+insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000001', '0000000014', 'ラストバージン', '歌詞', '250', 'music_data/0000000001.mp3', '0:5:13');
+insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000001', '0000000015', '針と棘', '歌詞', '250', 'music_data/0000000001.mp3', '0:7:20');
 
 -- function getUniqueStr(myStrong){
 --  var strong = 1000;
