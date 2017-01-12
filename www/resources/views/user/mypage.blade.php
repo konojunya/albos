720455b940d780b999fb0f127d01c8f5c467e3f5
konojunya <junya03dance@gmail.com>
2017-01-12 17:56:43 +0900

    mypage できた！

--- www/resources/views/user/mypage.blade.php
+++ www/resources/views/user/mypage.blade.php
@@ -12,8 +12,84 @@
 <main class="mdl-layout__content">
   <div class="page-content" id="app">
 
-    aa
+    <div class="wrapper clearfix">
+        <div class="left-box">
+            <h1 class="user-id load"></h1>
+            <p class="user-email load"></p>
 
+            <a href="/mypage/edit" class="settings">設定変更</a>
+
+        </div>
+        <div class="right-box">
+            <p class="buy-history">購入履歴</p>
+
+            <table class="mdl-data-table mdl-js-data-table mdl-data-table--selectable mdl-shadow--2dp music-table _hidden">
+                <thead>
+                  <tr>
+                    <th class="mdl-data-table__cell--non-numeric title">曲名</th>
+                    <th class="album-name">アルバム名</th>
+                    <th class="artist-name">アーティスト名</th>
+                  </tr>
+                </thead>
+                <tbody class="tbody"></tbody>
+            </table>
+
+        </div>
+    </div>
+    
   </div>
 </main>
+@endsection
+@section("javascript")
+<script>
+    (function($){
+
+        var test = [
+            {
+                music_id: "000000001",
+                album_title: "×と◯と罪と",
+                band_name: "RADWIMPS",
+                music_title: "いえない",
+                price: "250",
+                purchase_date: "2017-01-02 23:58:32"
+            },
+            {
+                music_id: "000000001",
+                album_title: "×と◯と罪と",
+                band_name: "RADWIMPS",
+                music_title: "いえない",
+                price: "250",
+                purchase_date: "2017-01-02 23:58:32"
+            },
+        ]
+
+        $.ajax({
+            url: "/api/user/home",
+            type: "GET"
+        })
+        .done(function(data){
+            $(".user-id").removeClass("load").text(data.user_id)
+            $(".user-email").removeClass("load").text(data.email)
+            initHistoryData(test)
+        })
+
+        function initHistoryData(data){
+
+            if(data.length > 0){
+                $(".music-table").show()
+                data.map(function(item){
+                    $(".tbody").append("<tr class='buy-item' data-id='"+item.music_id+"'><td class='mdl-data-table__cell--non-numeric'>"+item.music_title+"</td><td>"+item.album_title+"</td><td>"+item.band_name+"</td></tr>")
+                })
+            }else{
+                $(".right-box").append("<p class='no-history'>購入したものはありません。<br/><br/><a href='/music'>音楽を探しに行きましょう！</a></p>")
+            }
+        }
+
+        $(".music-table").on("click",".buy-item",function(){
+            var id = $(this).data("id")
+            location.pathname = "/music/"+id
+        })
+
+    })(jQuery)
+</script>
 @endsection
