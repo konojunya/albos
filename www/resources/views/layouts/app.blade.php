16b12e5c8be0e304c593f669c8155e52f16f9c52
konojunya <junya03dance@gmail.com>
2017-01-12 15:13:44 +0900

    app.blade.phpを書き換え

--- www/resources/views/layouts/app.blade.php
+++ www/resources/views/layouts/app.blade.php
@@ -44,39 +44,6 @@
                 </nav>
             </div>
         </header>
-<<<<<<< HEAD
-    <!-- <nav class="navbar navbar-default navbar-static-top">
-        <div class="container">
-            <div class="navbar-header">                
-                <a class="navbar-brand" href="{{ url('/') }}">
-                albos
-                </a>
-            </div>
-
-            <div class="collapse navbar-collapse" id="app-navbar-collapse"> -->
-                <!-- Right Side Of Navbar -->
-                <!-- <ul class="nav navbar-nav navbar-right"> -->
-                    <!-- Authentication Links -->
-                    <!-- @if (Auth::guest())
-                        <li><a href="{{ url('/login') }}">Login</a></li>
-                        <li><a href="{{ url('/register') }}">Register</a></li>
-                    @else
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                {{ Auth::user()->user_id }} <span class="caret"></span>
-                            </a>
-
-                            <ul class="dropdown-menu" role="menu">
-                                <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
-                            </ul>
-                        </li>
-                    @endif
-                </ul>
-            </div>
-        </div>
-    </nav> -->
-=======
->>>>>>> 510575799d4069c42a0c10df81f18d9736368635
 
     @yield('content')
 
