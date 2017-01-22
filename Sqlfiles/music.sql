1f7e205e1c2437f5777fe2f76a58d8884b98660b
kinokoruumu <kinokoruumu416@yahoo.co.jp>
2017-01-22 10:54:09 +0900

    music.sql修正しました

--- Sqlfiles/music.sql
+++ Sqlfiles/music.sql
@@ -14,44 +14,72 @@ insert into music (album_id, music_id, music_title, lyrics, price, music_data_pa
 insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000001', '0000000014', 'ラストバージン', '歌詞', '250', 'music_data/0000000001.mp3', '0:5:13');
 insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000001', '0000000015', '針と棘', '歌詞', '250', 'music_data/0000000001.mp3', '0:7:20');
 
-
-insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000002', '0000000016', 'DON\'T LOSE YOURSELF', '歌詞', '250', 'music_data/0000000001.mp3', '00:04:35');
-insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000002', '0000000017', '1997', '歌詞', '250', 'music_data/0000000001.mp3', '00:03:49');
-insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000002', '0000000018', 'PANORAMA RADIO', '歌詞', '250', 'music_data/0000000001.mp3', '00:04:29');
-insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000002', '0000000019', 'TAKE ME HOME', '歌詞', '250', 'music_data/0000000001.mp3', '0:5:19');
-insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000002', '0000000020', 'DARMA GRAND PRIX', '歌詞', '250', 'music_data/0000000001.mp3', '00:02:14');
-insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000002', '0000000021', 'WELCOME TO THE NEWWORLD', '歌詞', '250', 'music_data/0000000001.mp3', '00:02:51');
-insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000002', '0000000022', 'モラトリアム', '歌詞', '250', 'music_data/0000000001.mp3', '00:04:52');
-insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000002', '0000000023', 'DON\'T LOSE YOURSELF ～FXXKIN\' Mix', '歌詞', '250', 'music_data/0000000001.mp3', '00:05:48');
-
-
-insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000003', '0000000024', 'Survivor', '歌詞', '250', 'music_data/0000000001.mp3', '00:04:31');
-insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000003', '0000000025', 'Waiting for the Moment', '歌詞', '250', 'music_data/0000000001.mp3', '00:03:59');
-insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000003', '0000000026', 'Dive', '歌詞', '250', 'music_data/0000000001.mp3', '00:04:25');
-insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000003', '0000000027', 'Raise your flag', '歌詞', '250', 'music_data/0000000001.mp3', '00:04:07');
-insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000003', '0000000028', 'followers', '歌詞', '250', 'music_data/0000000001.mp3', '00:04:23');
-insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000003', '0000000029', 'The World\'s On Fire', '歌詞', '250', 'music_data/0000000001.mp3', '00:04:07');
-insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000003', '0000000030', 'Give it Away', '歌詞', '250', 'music_data/0000000001.mp3', '00:03:11');
-insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000003', '0000000031', 'Seven Deadly Sins', '歌詞', '250', 'music_data/0000000001.mp3', '00:03:27');
-insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000003', '0000000032', 'Mirror Mirror', '歌詞', '250', 'music_data/0000000001.mp3', '00:03:28');
-insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000003', '0000000033', 'Memories', '歌詞', '250', 'music_data/0000000001.mp3', '00:04:40');
-insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000003', '0000000034', 'Far', '歌詞', '250', 'music_data/0000000001.mp3', '00:04:09');
-insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000003', '0000000035', 'Out of Control', '歌詞', '250', 'music_data/0000000001.mp3', '00:03:20');
-insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000003', '0000000036', 'ワンダーランド', '歌詞', '250', 'music_data/0000000001.mp3', '00:04:43');
-
-
-insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000004', '0000000037', 'DANCE EVERYBODY', '歌詞', '250', 'music_data/0000000001.mp3', '00:03:46');
-insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000004', '0000000038', 'FLY AGAIN', '歌詞', '250', 'music_data/0000000001.mp3', '00:04:10');
-insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000004', '0000000039', 'SCENT OF YESTERDAY', '歌詞', '250', 'music_data/0000000001.mp3', '00:04:16');
-insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000004', '0000000040', 'NEVER FXXKIN\' MIND THE RULES', '歌詞', '250', 'music_data/0000000001.mp3', '00:03:11');
-insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000004', '0000000041', 'RAIN OF JULY', '歌詞', '250', 'music_data/0000000001.mp3', '00:05:18');
+insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000002', '0000000016', '夢灯籠', '歌詞', '250', 'music_data/0000000001.mp3', '0:2:08');
+insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000002', '0000000017', '三葉の通学', '歌詞', '250', 'music_data/0000000001.mp3', '0:1:06');
+insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000002', '0000000018', '糸守高校', '歌詞', '250', 'music_data/0000000001.mp3', '0:1:48');
+insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000002', '0000000019', 'はじめての、東京', '歌詞', '250', 'music_data/0000000001.mp3', '0:1:16');
+insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000002', '0000000020', '憧れカフェ', '歌詞', '250', 'music_data/0000000001.mp3', '0:1:29');
+insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000002', '0000000021', '奥寺先輩のテーマ', '歌詞', '250', 'music_data/0000000001.mp3', '0:2:05');
+insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000002', '0000000022', 'ふたりの異変', '歌詞', '250', 'music_data/0000000001.mp3', '0:2:36');
+insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000002', '0000000023', '前前前世 (movie ver.)', '歌詞', '250', 'music_data/0000000001.mp3', '0:4:44');
+insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000002', '0000000024', '御神体', '歌詞', '250', 'music_data/0000000001.mp3', '0:3:24');
+insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000002', '0000000025', 'デート', '歌詞', '250', 'music_data/0000000001.mp3', '0:4:03');
+insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000002', '0000000026', '秋祭り', '歌詞', '250', 'music_data/0000000001.mp3', '0:1:06');
+insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000002', '0000000027', '記憶を呼び起こす瀧', '歌詞', '250', 'music_data/0000000001.mp3', '0:1:40');
+insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000002', '0000000028', '飛騨探訪', '歌詞', '250', 'music_data/0000000001.mp3', '0:1:28');
+insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000002', '0000000029', '消えた町', '歌詞', '250', 'music_data/0000000001.mp3', '0:1:55');
+insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000002', '0000000030', '図書館', '歌詞', '250', 'music_data/0000000001.mp3', '0:1:41');
+insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000002', '0000000031', '旅館の夜', '歌詞', '250', 'music_data/0000000001.mp3', '0:1:29');
+insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000002', '0000000032', '御神体へ再び', '歌詞', '250', 'music_data/0000000001.mp3', '0:2:20');
+insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000002', '0000000033', '口噛み酒トリップ', '歌詞', '250', 'music_data/0000000001.mp3', '0:02:54');
+insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000002', '0000000034', '作戦会議', '歌詞', '250', 'music_data/0000000001.mp3', '0:02:20');
+insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000002', '0000000035', '町長説得', '歌詞', '250', 'music_data/0000000001.mp3', '0:02:45');
+insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000002', '0000000036', '三葉のテーマ', '歌詞', '250', 'music_data/0000000001.mp3', '0:4:02');
+insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000002', '0000000037', '見えないふたり', '歌詞', '250', 'music_data/0000000001.mp3', '0:0:50');
+insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000002', '0000000038', 'かたわれ時', '歌詞', '250', 'music_data/0000000001.mp3', '0:02:47');
+insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000002', '0000000039', 'スパークル (movie ver.)', '歌詞', '250', 'music_data/0000000001.mp3', '0:08:53');
+insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000002', '0000000040', 'デート2', '歌詞', '250', 'music_data/0000000001.mp3', '0:02:08');
+insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000002', '0000000041', 'なんでもないや (movie edit)', '歌詞', '250', 'music_data/0000000001.mp3', '0:03:14');
+insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000002', '0000000042', 'なんでもないや (movie ver.)', '歌詞', '250', 'music_data/0000000001.mp3', '0:05:43');
 
 
-insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000005', '0000000042', 'tales of purefly', '歌詞', '250', 'music_data/0000000001.mp3', '00:03:46');
-insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000005', '0000000043', 'evils fall', '歌詞', '250', 'music_data/0000000001.mp3', '00:04:10');
-insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000005', '0000000044', 'Wake Myself Again', '歌詞', '250', 'music_data/0000000001.mp3', '00:04:16');
-insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000005', '0000000045', 'database feat.TAKUMA(10-FEET)', '歌詞', '250', 'music_data/0000000001.mp3', '00:03:11');
-insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000005', '0000000046', 'vitamin 64', '歌詞', '250', 'music_data/0000000001.mp3', '00:05:18');
+insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000003', '0000000043', 'DON\'T LOSE YOURSELF', '歌詞', '250', 'music_data/0000000001.mp3', '00:04:35');
+insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000003', '0000000044', '1997', '歌詞', '250', 'music_data/0000000001.mp3', '00:03:49');
+insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000003', '0000000045', 'PANORAMA RADIO', '歌詞', '250', 'music_data/0000000001.mp3', '00:04:29');
+insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000003', '0000000046', 'TAKE ME HOME', '歌詞', '250', 'music_data/0000000001.mp3', '0:5:19');
+insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000003', '0000000047', 'DARMA GRAND PRIX', '歌詞', '250', 'music_data/0000000001.mp3', '00:02:14');
+insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000003', '0000000048', 'WELCOME TO THE NEWWORLD', '歌詞', '250', 'music_data/0000000001.mp3', '00:02:51');
+insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000003', '0000000049', 'モラトリアム', '歌詞', '250', 'music_data/0000000001.mp3', '00:04:52');
+insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000003', '0000000050', 'DON\'T LOSE YOURSELF ～FXXKIN\' Mix', '歌詞', '250', 'music_data/0000000001.mp3', '00:05:48');
+
+
+insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000004', '0000000051', 'Survivor', '歌詞', '250', 'music_data/0000000001.mp3', '00:04:31');
+insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000004', '0000000052', 'Waiting for the Moment', '歌詞', '250', 'music_data/0000000001.mp3', '00:03:59');
+insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000004', '0000000053', 'Dive', '歌詞', '250', 'music_data/0000000001.mp3', '00:04:25');
+insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000004', '0000000054', 'Raise your flag', '歌詞', '250', 'music_data/0000000001.mp3', '00:04:07');
+insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000004', '0000000055', 'followers', '歌詞', '250', 'music_data/0000000001.mp3', '00:04:23');
+insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000004', '0000000056', 'The World\'s On Fire', '歌詞', '250', 'music_data/0000000001.mp3', '00:04:07');
+insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000004', '0000000057', 'Give it Away', '歌詞', '250', 'music_data/0000000001.mp3', '00:03:11');
+insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000004', '0000000058', 'Seven Deadly Sins', '歌詞', '250', 'music_data/0000000001.mp3', '00:03:27');
+insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000004', '0000000059', 'Mirror Mirror', '歌詞', '250', 'music_data/0000000001.mp3', '00:03:28');
+insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000004', '0000000060', 'Memories', '歌詞', '250', 'music_data/0000000001.mp3', '00:04:40');
+insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000004', '0000000061', 'Far', '歌詞', '250', 'music_data/0000000001.mp3', '00:04:09');
+insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000004', '0000000062', 'Out of Control', '歌詞', '250', 'music_data/0000000001.mp3', '00:03:20');
+insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000004', '0000000063', 'ワンダーランド', '歌詞', '250', 'music_data/0000000001.mp3', '00:04:43');
+
+
+insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000005', '0000000064', 'DANCE EVERYBODY', '歌詞', '250', 'music_data/0000000001.mp3', '00:03:46');
+insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000005', '0000000065', 'FLY AGAIN', '歌詞', '250', 'music_data/0000000001.mp3', '00:04:10');
+insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000005', '0000000066', 'SCENT OF YESTERDAY', '歌詞', '250', 'music_data/0000000001.mp3', '00:04:16');
+insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000005', '0000000067', 'NEVER FXXKIN\' MIND THE RULES', '歌詞', '250', 'music_data/0000000001.mp3', '00:03:11');
+insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000005', '0000000068', 'RAIN OF JULY', '歌詞', '250', 'music_data/0000000001.mp3', '00:05:18');
+
+
+insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000006', '0000000069', 'tales of purefly', '歌詞', '250', 'music_data/0000000001.mp3', '00:03:46');
+insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000006', '0000000070', 'evils fall', '歌詞', '250', 'music_data/0000000001.mp3', '00:04:10');
+insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000006', '0000000071', 'Wake Myself Again', '歌詞', '250', 'music_data/0000000001.mp3', '00:04:16');
+insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000006', '0000000072', 'database feat.TAKUMA(10-FEET)', '歌詞', '250', 'music_data/0000000001.mp3', '00:03:11');
+insert into music (album_id, music_id, music_title, lyrics, price, music_data_path, music_time) values ('0000000006', '0000000073', 'vitamin 64', '歌詞', '250', 'music_data/0000000001.mp3', '00:05:18');
 
 -- function getUniqueStr(myStrong){
 --  var strong = 1000;
