aaffc765c8054709035754bd3c6b723e4787231e
konojunya <junya03dance@gmail.com>
2017-01-24 16:07:36 +0900

    login view変更

--- www/resources/views/auth/login.blade.php
+++ www/resources/views/auth/login.blade.php
@@ -25,19 +25,17 @@ login | albos
 			   		<table>
 			   			<tr>
 			   				<td>
-			   					ユーザーID
+			   					メールアドレス
 			   				</td>
 			   				<td>
 			   					<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label login-form-user-id">
-								   	<!-- <input class="mdl-textfield__input" type="text" id="user_id" name="user_id" placeholder="入力してください"> -->
-								   	<input id="email" type="text" class="mdl-textfield__input" name="email" value="{{ old('email') }}">
-								   	<!-- <label class="mdl-textfield__label" for="user-id">入力してください</label> -->
-								</div>
-								@if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
+			   						<input id="email" type="text" class="mdl-textfield__input" name="email" value="{{ old('email') }}">
+								  </div>
+									@if ($errors->has('email'))
+									<span class="help-block">
+										<strong>{{ $errors->first('email') }}</strong>
+									</span>
+									@endif
 			   				</td>
 			   			</tr>
 			   			<tr>
@@ -46,16 +44,14 @@ login | albos
 			   				</td>
 			   				<td>
 			   					<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label login-form-password">
-								   	<!-- <input class="mdl-textfield__input" type="password" id="password" name="password" placeholder="入力してください"> -->
-								   	<input id="password" type="password" class="mdl-textfield__input" name="password">
-								   	<!-- <label class="mdl-textfield__label" for="password">入力してください</label> -->
-								</div>
-								@if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-			   				</td>
+			   						<input id="password" type="password" class="mdl-textfield__input" name="password">
+									</div>
+									@if ($errors->has('password'))
+										<span class="help-block">
+											<strong>{{ $errors->first('password') }}</strong>
+										</span>
+									@endif
+				   			</td>
 			   			</tr>
 			   		</table>
 
@@ -63,7 +59,7 @@ login | albos
 
 			   		<div class="mdl-card__actions mdl-card--border">
 			   			<input type="submit" value="ログインする" class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect login-btn">
-			   			<a class="btn btn-link" href="{{ url('/password/reset') }}">Forgot Your Password?</a>
+			   			<a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect" href="{{ url('/password/reset') }}" style="text-decoration: none;">パスワードをお忘れですか？</a>
 					</div>
 						
 				</form>
@@ -72,37 +68,6 @@ login | albos
 			</div>
 		</div>
 
-
-		<div class="sign-up">
-			<div class="mdl-card mdl-shadow--2dp left-card">
-				<div class="mdl-card__title mdl-card--expand">
-					<h2 class="mdl-card__title-text">ユーザー登録</h2>
-				</div>
-				<div class="mdl-card__supporting-text">
-			   	曲を購入する為には<br>
-			   	ユーザー登録が必要です。
-				</div>
-				<div class="mdl-card__actions mdl-card--border">
-				   <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect" href="{{ url('/register') }}">
-				      ユーザー登録画面へ
-				   </a>
-				</div>
-			</div>
-			<div class="mdl-card mdl-shadow--2dp right-card">
-				<div class="mdl-card__title mdl-card--expand">
-					<h2 class="mdl-card__title-text">バンド登録</h2>
-				</div>
-				<div class="mdl-card__supporting-text">
-			   	バンド登録をすると、<br>
-			   	このサイトで曲を販売することが出来ます。
-				</div>
-				<div class="mdl-card__actions mdl-card--border">
-				   <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
-				      バンド登録画面へ
-				   </a>
-				</div>
-			</div>
-		</div>
 	</div>
 </main>
 @endsection
